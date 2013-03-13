560ead9b31c94d16ef52f29e76dd66d3064d610c
Mikhail <micam1818@gmail.com>
2013-03-13 12:48:23 +0900

    script to build x264 for Android ARM

---
+++ build_x264_arm.sh
@@ -0,0 +1,17 @@
+NDK=/your/path/to/android-ndk
+PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.4.3/prebuilt
+PLATFORM=$NDK/platforms/android-14/arch-arm
+PREFIX=/home/android-ffmpeg
+
+./configure --prefix=$PREFIX \
+--enable-static \
+--enable-pic \
+--disable-asm \
+--disable-cli \
+--host=arm-linux \
+--cross-prefix=$PREBUILT/linux-x86/bin/arm-linux-androideabi- \
+--sysroot=$PLATFORM
+
+make
+sudo make install
+sudo ldconfig
